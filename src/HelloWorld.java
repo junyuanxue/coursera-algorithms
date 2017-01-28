f192eb1de5a4151fab4dcd95aace472c3802d920
Junyuan Xue <junyuanxue.is@gmail.com>
2017-01-28 14:52:20 +0000

    Moving percolation under src

--- src/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by junyuanxue on 25/01/2017.
- */
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
-    }
-}
---
+++ src/percolation/Percolation.java
@@ -0,0 +1,31 @@
+public class Percolation {
+    public int n;
+
+    public Percolation(int n) { // n by n grid
+        n = n;
+    }
+
+    public void open(int row, int col) {
+
+    }
+
+    public boolean isOpen(int row, int col)  {
+
+    }
+
+    public boolean isFull(int row, int col) {
+
+    }
+
+    public int numberOfOpenSites() {
+
+    }
+
+    public boolean percolates() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
