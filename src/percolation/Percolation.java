c2114fb71c9ef96c40e6e6df3497240895d0ee8b
Junyuan Xue <junyuanxue.is@gmail.com>
2017-01-28 18:32:56 +0000

    Adding isInBounds private method

--- src/percolation/Percolation.java
+++ src/percolation/Percolation.java
@@ -1,14 +1,19 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    private int n = 0;
-    private WeightedQuickUnionUF;
+    private static int N;
+    private WeightedQuickUnionUF unionFind;
 
-    public Percolation(int n) { // n by n grid
-        n = n;
+    public Percolation(int n) { // create n-by-n grid, with all sites blocked
+        N = n;
+        unionFind = new WeightedQuickUnionUF(N * N + 2);
     }
 
-    public void open(int row, int col) {
+    public void open(int row, int col) { // open site (row, col) if it is not open already
+        if (isInBounds(row, col) && !isOpen(row, col)) {
+            
+        }
+
 
     }
 
@@ -30,5 +35,12 @@ public class Percolation {
 
     public static void main(String[] args) {
 
+    }
+
+    private boolean isInBounds(int row, int col) {
+        if (row < 1 || row > n || col < 1 || col > n) {
+            throw new IndexOutOfBoundsException();
+        }
+        return true;
     }
 }
