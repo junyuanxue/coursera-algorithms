1137856fdff9c393629ab932b8eb522bc0da6ac5
Junyuan Xue <junyuanxue.is@gmail.com>
2017-01-28 21:22:34 +0000

    Writing constructor for PercolationStats

--- src/percolation/PercolationStats.java
+++ src/percolation/PercolationStats.java
@@ -1,6 +1,17 @@
 public class PercolationStats {
-    public PercolationStats(int n, int trials) { // perform trials independent experiments on an n-by-n grid
+    private int trials;
+    private double[] thresholds;
 
+    public PercolationStats(int n, int numOfTrials) { // perform trials independent experiments on an n-by-n grid
+        if (n < 1 || trials < 1) {
+            throw new IllegalArgumentException();
+        }
+        trials = numOfTrials;
+        thresholds = new double[trials];
+        for (int i = 0; i < thresholds.length; i++)
+        {
+            thresholds[i] = calculateThreshold(n);
+        }
     }
 
     public double mean() { // sample mean of percolation threshold
@@ -16,6 +27,10 @@ public class PercolationStats {
     }
 
     public double confidenceHi() { // high endpoint of 95% confidence interval
+
+    }
+
+    private double calculateThreshold(int n) {
 
     }
 
