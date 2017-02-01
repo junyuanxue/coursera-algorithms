a0493ff7d221d0e7039e2ad56f7b9bbf05e1adf3
Junyuan Xue <junyuanxue.is@gmail.com>
2017-02-01 22:50:03 +0000

    Adding isEmpty and size functions

--- src/dequesAndRandomizedQueues/Deque.java
+++ src/dequesAndRandomizedQueues/Deque.java
@@ -1,10 +1,6 @@
 import java.util.Iterator;
 
 public class Deque<Item> implements Iterable<Item> {
-    private Node first;
-    private Node last;
-    private int size;
-
     /**
      * Private inner node class
      * Has next and prev pointers to implement constant worst time operations
@@ -15,14 +11,24 @@ public class Deque<Item> implements Iterable<Item> {
         public Node prev = null;
     }
 
+    private Node first;
+    private Node last;
+    private int size;
+
     public Deque() { // constructs an empty deque
         size = 0;
         first = null;
         last = null;
     }
+
+    public boolean isEmpty() { // is the deque empty?
+        return size === 0;
+    }
+
+    public int size() { // return the number of items on the deque
+        return size;
+    }
     
-    public boolean isEmpty()                 // is the deque empty?
-    public int size()                        // return the number of items on the deque
     public void addFirst(Item item)          // add the item to the front
     public void addLast(Item item)           // add the item to the end
     public Item removeFirst()                // remove and return the item from the front
