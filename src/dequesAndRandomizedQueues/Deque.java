31bf7ed4c27091c49f8844c4fdedb4d351cb7c3d
Junyuan Xue <junyuanxue.is@gmail.com>
2017-02-01 22:46:17 +0000

    Constructs an empty deque

--- src/dequesAndRandomizedQueues/Deque.java
+++ src/dequesAndRandomizedQueues/Deque.java
@@ -3,27 +3,24 @@ import java.util.Iterator;
 public class Deque<Item> implements Iterable<Item> {
     private Node first;
     private Node last;
-    private int numOfItems;
+    private int size;
 
     /**
      * Private inner node class
      * Has next and prev pointers to implement constant worst time operations
      */
     private class Node {
-      private Item item;
-      private Node next;
-      private Node prev;
-
-      Node(Item item) {
-        this.item = item;
-        this.next = null;
-        this.prev = null;
-      }
+        public Item value = null;
+        public Node next = null;
+        public Node prev = null;
     }
 
     public Deque() { // constructs an empty deque
-
+        size = 0;
+        first = null;
+        last = null;
     }
+    
     public boolean isEmpty()                 // is the deque empty?
     public int size()                        // return the number of items on the deque
     public void addFirst(Item item)          // add the item to the front
