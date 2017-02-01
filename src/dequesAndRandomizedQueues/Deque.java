b7ecad9fa2056455c3e0a7fe4be1b8f5bf45637c
Junyuan Xue <junyuanxue.is@gmail.com>
2017-02-01 22:59:35 +0000

    Adding an item to the front of the queue

--- src/dequesAndRandomizedQueues/Deque.java
+++ src/dequesAndRandomizedQueues/Deque.java
@@ -28,9 +28,25 @@ public class Deque<Item> implements Iterable<Item> {
     public int size() { // return the number of items on the deque
         return size;
     }
+
+    public void addFirst(Item item) { // add the item to the front
+        if (item == null) throw new java.lang.NullPointerException();
+
+        Node currentNode = new Node();
+        currentNode.value = item;
+        currentNode.next = first;
+        if (first != null) first.prev = currentNode; // setting the relationship between the first node and currentNode
+
+        first = currentNode; // currentNode becomes the first node
+        if (last == null) last = first; // when currentNode gets inserted into an empty array
+
+        size++;
+    }
+
+    public void addLast(Item item) { // add the item to the end
+
+    }
     
-    public void addFirst(Item item)          // add the item to the front
-    public void addLast(Item item)           // add the item to the end
     public Item removeFirst()                // remove and return the item from the front
     public Item removeLast()                 // remove and return the item from the end
     public Iterator<Item> iterator()         // return an iterator over items in order from front to end
