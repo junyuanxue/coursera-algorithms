d8b9018160249e733567bd6b1181017260f87f64
Junyuan Xue <junyuanxue.is@gmail.com>
2017-02-04 20:51:55 +0000

    Added enqueue function and resize private method

--- src/dequesAndRandomizedQueues/RandomizedQueue.java
+++ src/dequesAndRandomizedQueues/RandomizedQueue.java
@@ -14,8 +14,21 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     public int size() { // return the number of items on the queue
         return size;
     }
-    
-    public void enqueue(Item item)           // add the item
+
+    public void enqueue(Item item) { // add the item
+        if (item == null) throw new java.lang.NullPointerException();
+        if (size == queue.length) doubleQueueSize();
+        queue[size] = item;
+        size++;
+    }
+
+    private void doubleQueueSize () {
+        private int currentSize = queue.length
+        Item[] resizedQueue = (Item[]) new Object[currentSize * 2];
+        System.arraycopy(queue, 0, resizedQueue, 0, currentSize);
+        queue = resizedQueue;
+    }
+
     public Item dequeue()                    // remove and return a random item
     public Item sample()                     // return (but do not remove) a random item
     public Iterator<Item> iterator()         // return an independent iterator over items in random order
