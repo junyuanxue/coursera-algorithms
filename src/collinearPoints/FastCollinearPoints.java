59c9a12af0e99f739c13c51855f9f21b810074a9
Junyuan Xue <junyuanxue.is@gmail.com>
2017-02-09 18:28:08 +0000

    Figuring out Fast...

--- src/collinearPoints/FastCollinearPoints.java
+++ src/collinearPoints/FastCollinearPoints.java
@@ -1,8 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+/**
+ * A fast solution of finding colinear points in a set of points.
+ */
+
 public class FastCollinearPoints {
     private LineSegment[] segments;
 
     public FastCollinearPoints(Point[] points) { // finds all line segments containing 4 or more points
+        checkNoDuplicatedPoints(points);
 
+
+
+    }
+
+    private void banana (Point[] points) {
+        Point[] pointsCopy = Arrays.copyOf(points, points.length);
+
+        for (Point startPoint : points) {
+            Arrays.sort(pointsCopy, startPoint.slopeOrder());
+            List<Point> slopePoints = new ArrayList<>();
+            double slope = 0;
+            double previousSlope = Double.NEGATIVE_INFINITY;
+
+            for (int i = 1; i < pointsCopy.length; i++) {
+                Point otherPoint = pointsCopy[i]
+                slope = startPoint.slopeTo(otherPoint);
+                if (slope == previousSlope) {
+                    slopePoints.add(otherPoint);
+                } else {
+                    if (slopePoints.size() >= 3) {
+                        slopePoints.add(startPoint);
+                        addSegmentIfNew(slopePoints, previousSlope);
+                    }
+                    slopePoints.clear();
+                    slopePoints.add(otherPoint);
+                }
+                previousSlope = slope;
+            }
+
+            if (slopePoints.size() >= 3) {
+                slopePoints.add(startPoint);
+                addSegmentIfNew(slopePoints, previousSlope);
+            }
+        }
     }
 
     public int numberOfSegments() { // the number of line segments
@@ -11,5 +54,15 @@ public class FastCollinearPoints {
 
     public LineSegment[] segments() { // the line segments
         return segments;
+    }
+
+    private void checkNoDuplicatedPoints(Point[] points) {
+        for (i = 0; i < points.length; i++) {
+            for (j = i + 1; j < points.length; j++) {
+                if (points[i].compareTo(points[j]) == 0) {
+                    throw new IllegalArgumentException("Duplicated points in input.");
+                }
+            }
+        }
     }
 }
--- src/collinearPoints/Point.java
+++ src/collinearPoints/Point.java
@@ -69,10 +69,13 @@ public class Point implements Comparable<Point> {
     /**
      * Returns the slope between this point and the specified point.
      * Formally, if the two points are (x0, y0) and (x1, y1), then the slope
-     * is (y1 - y0) / (x1 - x0). For completeness, the slope is defined to be
-     * +0.0 if the line segment connecting the two points is horizontal;
+     * is (y1 - y0) / (x1 - x0).
+     * For completeness, the slope is defined to be +0.0 if
+     * the line segment connecting the two points is horizontal;
+     *
      * Double.POSITIVE_INFINITY if the line segment is vertical;
-     * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
+     *
+     * Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
      *
      * @param  that the other point
      * @return the slope between this point and the specified point
