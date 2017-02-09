a1f2b2b7b3de091f6591af22b238b4d7a6189894
Junyuan Xue <junyuanxue.is@gmail.com>
2017-02-09 20:05:10 +0000

    Modified imports

--- src/collinearPoints/FastCollinearPoints.java
+++ src/collinearPoints/FastCollinearPoints.java
@@ -1,4 +1,7 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
 
 /**
  * A fast solution of finding colinear points in a set of points.
