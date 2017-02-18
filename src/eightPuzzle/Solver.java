2aa1caf0bd2a8e119a40cc2f44c000ce68a6fffb
Junyuan Xue <junyuanxue.is@gmail.com>
2017-02-18 15:04:06 +0000

    passing tester. removed old code and added comments

--- src/eightPuzzle/Solver.java
+++ src/eightPuzzle/Solver.java
@@ -27,6 +27,9 @@ public class Solver {
 
     private Move lastMove;
 
+    /**
+     * find a solution to the initial board (using the A* algorithm)
+     */
     public Solver(Board initial) {
         MinPQ<Move> moves = new MinPQ<Move>();
         moves.insert(new Move(initial));
@@ -52,14 +55,26 @@ public class Solver {
         return null;
     }
 
+    /**
+     * is the initial board solvable?
+     */
     public boolean isSolvable() {
         return (lastMove != null);
     }
 
+    /**
+     * min number of moves to solve initial board; -1 if unsolvable
+     */
     public int moves() {
-        return isSolvable() ? lastMove.numOfMoves : -1;
+        if (isSolvable()) {
+            return lastMove.numOfMoves;
+        }
+        return -1;
     }
 
+    /**
+     * sequence of boards in a shortest solution; null if unsolvable
+     */
     public Iterable<Board> solution() {
         if (!isSolvable()) return null;
 
@@ -72,6 +87,9 @@ public class Solver {
         return moves;
     }
 
+    /**
+     *  solve a slider puzzle
+     */
     public static void main(String[] args) {
         // create initial board from file
         In in = new In(args[0]);
@@ -94,110 +112,4 @@ public class Solver {
                 StdOut.println(board);
         }
     }
-
-//    private MinPQ<Node> queue;
-//    private MinPQ<Node> swapQueue;
-//    private boolean solvable;
-//    private boolean swapSolvable;
-//    private Node endNode;
-//
-//    private class Node {
-//        private Board board;
-//        private Node parent;
-//
-//        public Node(Board board, Node parent) {
-//            this.board = board;
-//            this.parent = parent;
-//        }
-//    }
-//
-//    private static Comparator<Node> boardComparator = new Comparator<Node>() {
-//        @Override
-//        public int compare(Node first, Node second) {
-//            return first.board.manhattan() - second.board.manhattan();
-//        }
-//    };
-//
-//    /**
-//     * find a solution to the initial board (using the A* algorithm)
-//     */
-//    public Solver(Board initial) {
-//        solvable = false;
-//        swapSolvable = false;
-//
-//        queue = new MinPQ<Node>(boardComparator);
-//        swapQueue = new MinPQ<Node>(boardComparator);
-//
-//        Node initialNode = new Node(initial, null);
-//        queue.insert(initialNode);
-//
-//        Node initialSwap = new Node(initial.twin(), null);
-//        swapQueue.insert(initialSwap);
-//
-//        while (!solvable && !swapSolvable) {
-//            solvable = canSolveStep(queue);
-//            swapSolvable = canSolveStep(swapQueue);
-//        }
-//    }
-//
-//    private boolean canSolveStep(MinPQ<Node> queue) {
-//        // Pop min from queue, check if solved, if not, queue neighbours
-//        Node current = queue.delMin();
-//
-//        if (current.board.isGoal()) {
-//            endNode = current;
-//            return true;
-//        }
-//
-//        for (Board board : current.board.neighbors()) {
-//            if (current.parent == null || !board.equals(current.parent.board)) {
-//                Node neighbor = new Node(board, current);
-//                queue.insert(neighbor);
-//            }
-//        }
-//
-//        return false;
-//    }
-//
-//    /**
-//     * is the initial board solvable?
-//     */
-//    public boolean isSolvable() {
-//        return solvable;
-//    }
-//
-//    /**
-//     * min number of moves to solve initial board; -1 if unsolvable
-//     */
-//    public int moves() {
-//        if (isSolvable()) {
-//            Node current = endNode;
-//            int moves = 0;
-//            while (current.parent != null) {
-//                moves++;
-//                current = current.parent; // trace back to initial node
-//            }
-//            return moves;
-//        }
-//        return -1;
-//    }
-//
-//    /**
-//     * sequence of boards in a shortest solution; null if unsolvable
-//     */
-//    public Iterable<Board> solution() {
-//        if (isSolvable()) {
-//            Stack<Board> solution = new Stack<Board>();
-//            Node current = endNode;
-//            solution.push(endNode.board);
-//            while (current.parent != null) {
-//                solution.push(current.parent.board);
-//                current = current.parent;
-//            }
-//            return solution;
-//        }
-//        return null;
-//    }
-//
-
 }
