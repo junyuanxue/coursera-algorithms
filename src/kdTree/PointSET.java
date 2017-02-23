0394eda4af320928e434518c37f0e8efee31050b
Junyuan Xue <junyuanxue.is@gmail.com>
2017-02-23 22:31:34 +0000

    insert()

--- src/kdTree/PointSET.java
+++ src/kdTree/PointSET.java
@@ -1,5 +1,6 @@
 import java.util.LinkedList;
 import java.util.List;
+import java.lang.NullPointerException;
 
 public class PointSET {
     private SET<Point2D> points;
@@ -24,8 +25,21 @@ public class PointSET {
     public int size() {
         return points.size();
     }
-    public              void insert(Point2D p)              // add the point to the set (if it is not already in the set)
-    public           boolean contains(Point2D p)            // does the set contain point p?
+
+    /**
+     * add the point to the set (if it is not already in the set)
+     * */
+    public void insert(Point2D p) {
+        if (p == null) throw new NullPointerException();
+        points.add(p);
+    }
+
+    /**
+     * does the set contain point p?
+     * */
+    public boolean contains(Point2D p) {
+
+    }            
     public              void draw()                         // draw all points to standard draw
     public Iterable<Point2D> range(RectHV rect)             // all points that are inside the rectangle
     public           Point2D nearest(Point2D p)             // a nearest neighbor in the set to point p; null if the set is empty
